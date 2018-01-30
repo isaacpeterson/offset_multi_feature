99d65b083eed761f8692be4eec7f0b619b529edf
isaacpeterson <isaac.p.mail@gmail.com>
2018-01-30 12:50:51 +1100

    add param files for multi feature simulation

---
+++ run_offset_simulation_with_simulated_data.R
@@ -0,0 +1,13 @@
+library(offsetsim)
+
+source('simulated_data_params.R')
+
+user_simulation_params = initialise_user_simulation_params()
+user_global_params = initialise_user_global_params()
+user_simulated_ecology_params = initialise_user_simulated_ecology_params()
+user_plot_params <- initialise_user_plot_params()
+
+osim.run(user_global_params, user_simulation_params, user_simulated_ecology_params, loglevel = 'TRACE')
+
+#include run_number for specified run folder - leave to automatically select latest
+# osim.plot(user_plot_params, user_global_params$simulation_folder, loglevel = 'TRACE')
